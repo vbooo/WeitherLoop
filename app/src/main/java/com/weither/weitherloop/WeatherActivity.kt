f527ae987c36aa4c0772820e9b4e0ea5e67bafcf
fabien bouvet <fabien.bouvet78@gmail.com>
2021-09-19 01:31:33 +0200

    weather UI raw view

--- app/src/main/java/com/weither/weitherloop/WeatherActivity.kt
+++ app/src/main/java/com/weither/weitherloop/WeatherActivity.kt
@@ -2,12 +2,36 @@ package com.weither.weitherloop
 
 import android.os.Bundle
 import android.view.MenuItem
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.weither.weitherloop.databinding.ActivityMainBinding
+import com.weither.weitherloop.databinding.ActivityWeatherBinding
 
 class WeatherActivity : AppCompatActivity() {
+
+    private val viewModel: WeatherViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_weather)
+
+        val binding = ActivityWeatherBinding.inflate(layoutInflater)
+        val view = binding.root
+
+        viewModel.label?.observe(
+            this,
+            {
+                binding.activityWeatherLabel.text = it
+            }
+        )
+
+        viewModel.progress.observe(
+            this,
+            {
+                binding.activityWeatherDeterminateBar.progress = it.toInt()
+            }
+        )
+
+        setContentView(view)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
---
+++ app/src/main/java/com/weither/weitherloop/WeatherViewModel.kt
@@ -0,0 +1,69 @@
+package com.weither.weitherloop
+
+import android.os.CountDownTimer
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import java.util.*
+import kotlin.concurrent.schedule
+
+class WeatherViewModel : ViewModel() {
+
+    val listOfCity = arrayListOf("First", "Second", "Three")
+
+    var label: MutableLiveData<String>? = MutableLiveData("")
+    var progress: MutableLiveData<Double> = MutableLiveData(0.0)
+    var tabLabel = arrayListOf(
+        "Nous téléchargeons les données...",
+        "C’est presque fini...",
+        "Plus que quelques secondes avant d’avoir le résultat..."
+    )
+
+    init {
+        repeatLabel()
+        getCitiesWeather()
+        handleProgressBar()
+    }
+
+    private fun getCitiesWeather() {
+        var iterator = 0
+        Timer().schedule(
+            Date(), 10000
+        ) {
+
+            println("Call API " + listOfCity[iterator])
+            iterator++
+            if (iterator == listOfCity.size) {
+                this.cancel()
+                println("All cities has been called: timer canceled")
+            }
+        }
+    }
+
+    private fun handleProgressBar() {
+        object : CountDownTimer(60000, 1000) {
+
+            override fun onTick(millisUntilFinished: Long) {
+                progress.postValue(progress.value?.plus(1))
+            }
+
+            override fun onFinish() {
+                println("FINISH")
+            }
+        }.start()
+    }
+
+    private fun repeatLabel() {
+        viewModelScope.launch {
+            viewModelScope.launch {
+                for (item in tabLabel) {
+                    label?.postValue(item)
+                    delay(6000)
+                }
+                repeatLabel()
+            }
+        }
+    }
+}
